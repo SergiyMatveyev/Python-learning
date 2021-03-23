4342e2feaf0b46760bed8820136b2692f4e57b1b
SergiyMatveyev <sergiymtvwork@gmail.com>
2021-03-23 15:48:03 +0200

    Docstrings

---
+++ Docstings/docstings.py
@@ -0,0 +1,8 @@
+# A docstring is a string that occurs as the first statement in a module, function, class, or method definition. 
+# Such a docstring becomes the __doc__ special attribute of that object.
+
+class MyDocumentedClass:
+    """This class is well documented but doesn't do anything special."""
+    def do_nothing(self):
+        """This method doesn't do anything but feel free to call it anyway."""
+        pass
